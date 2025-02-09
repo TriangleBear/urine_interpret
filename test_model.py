0911db52d0fff2b41d75d9242f3637cad984dbd0
TriangleBear <kakanini31@gmail.com>
2025-02-09 22:25:43 +0800

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -87,7 +87,7 @@ class UNet(nn.Module):
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = UNet(in_channels=3, out_channels=10)
 
-checkpoint = torch.load('unet_model_20250209-001407.pth', map_location=device)
+checkpoint = torch.load('unet_model_20250209-121235.pth', map_location=device)
 
 # If the checkpoint contains a 'state_dict' key, use it; otherwise, use the checkpoint directly.
 if 'state_dict' in checkpoint:
