5e0e066305d58fc83d5f36b5c046da5eb8280c97
TriangleBear <kakanini31@gmail.com>
2025-02-20 14:51:29 +0800

    Update config.py

--- Train/config.py
+++ Train/config.py
@@ -7,8 +7,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Path Configuration
 BASE_PATH = r"D:\Programming\urine_interpret"
-IMAGE_FOLDER = os.path.join(BASE_PATH, r"Datasets\Final Dataset I think\images")
-MASK_FOLDER = os.path.join(BASE_PATH, r"Datasets\Final Dataset I think\labels")
+IMAGE_FOLDER = os.path.join(BASE_PATH, r"Datasets\Final Dataset I think\train\images")
+MASK_FOLDER = os.path.join(BASE_PATH, r"Datasets\Final Dataset I think\train\labels")
 
 # Training Hyperparameters
 BATCH_SIZE = 4  # Increased batch size since T4 has more VRAM
