9defde85cc815ff91266ea560d83327d376e0e30
TriangleBear <kakanini31@gmail.com>
2025-02-24 14:16:23 +0800

    Update losses.py

--- Train/losses.py
+++ Train/losses.py
@@ -5,6 +5,8 @@ def dice_loss(outputs, targets, smooth=1e-6):
     outputs = F.softmax(outputs, dim=1)
     if targets.dim() == 4:
         targets = targets.squeeze(1)
+    if targets.dim() == 3:
+        targets = targets.unsqueeze(1)
     targets_one_hot = F.one_hot(targets.long(), outputs.shape[1]).permute(0, 3, 1, 2).float()
     intersection = (outputs * targets_one_hot).sum(dim=(2,3))
     union = outputs.sum(dim=(2,3)) + targets_one_hot.sum(dim=(2,3))
@@ -14,8 +16,8 @@ def dice_loss(outputs, targets, smooth=1e-6):
 def focal_loss(outputs, targets, alpha=0.25, gamma=2):
     if targets.dim() == 4:
         targets = targets.squeeze(1)
-    if targets.dim() == 3:
-        targets = targets.unsqueeze(1)
+    if targets.dim() == 2:
+        targets = targets.unsqueeze(1).unsqueeze(2)
     targets_one_hot = F.one_hot(targets, outputs.shape[1]).permute(0, 3, 1, 2).float()
     bce_loss = F.binary_cross_entropy_with_logits(outputs, targets_one_hot, reduction='none')
     pt = torch.exp(-bce_loss)
