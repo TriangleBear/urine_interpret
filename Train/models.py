d4fe1ac4d273e6023cc3db71b16c76ac9475be07
TriangleBear <kakanini31@gmail.com>
2025-02-24 09:41:58 +0800

    Update models.py

--- Train/models.py
+++ Train/models.py
@@ -2,74 +2,22 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class UNet(nn.Module):
-    def __init__(self, in_channels, out_channels=11, dropout_prob=0.3):
-        super(UNet, self).__init__()
-        self.enc1 = nn.Sequential(
-            nn.Conv2d(in_channels, 32, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(negative_slope=0.01, inplace=True),
-            nn.Dropout(p=dropout_prob)
-        )
-        self.enc2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(negative_slope=0.01, inplace=True),
-            nn.Dropout(p=dropout_prob)
-        )
-        self.enc3 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(negative_slope=0.01, inplace=True),
-            nn.Dropout(p=dropout_prob)
-        )
-        self.enc4 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(negative_slope=0.01, inplace=True),
-            nn.Dropout(p=dropout_prob)
-        )
-        self.bottleneck = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(negative_slope=0.01, inplace=True),
-            nn.Dropout(p=dropout_prob)
-        )
-        self.upconv4 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.upconv3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.upconv2 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.upconv1 = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
-        self.conv_up4 = nn.Sequential(
-            nn.Conv2d(512, 256, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(negative_slope=0.01, inplace=True),
-            nn.Dropout(p=dropout_prob)
-        )
-        self.conv_up3 = nn.Sequential(
-            nn.Conv2d(256, 128, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(negative_slope=0.01, inplace=True),
-            nn.Dropout(p=dropout_prob)
-        )
-        self.conv_up2 = nn.Sequential(
-            nn.Conv2d(128, 64, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(negative_slope=0.01, inplace=True),
-            nn.Dropout(p=dropout_prob)
-        )
-        self.conv_up1 = nn.Sequential(
-            nn.Conv2d(64, 32, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(negative_slope=0.01, inplace=True),
-            nn.Dropout(p=dropout_prob)
-        )
-        self.output = nn.Conv2d(32, out_channels, kernel_size=1, bias=False)
-        self.initialize_weights()
+class UNetEncoder(nn.Module):
+    def __init__(self, in_channels, dropout_prob=0.3):
+        super(UNetEncoder, self).__init__()
+        self.enc1 = self.conv_block(in_channels, 32, dropout_prob)
+        self.enc2 = self.conv_block(32, 64, dropout_prob)
+        self.enc3 = self.conv_block(64, 128, dropout_prob)
+        self.enc4 = self.conv_block(128, 256, dropout_prob)
+        self.bottleneck = self.conv_block(256, 512, dropout_prob)
 
-    def initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d)):
-                nn.init.kaiming_uniform_(m.weight, nonlinearity='leaky_relu')
+    def conv_block(self, in_channels, out_channels, dropout_prob):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(negative_slope=0.01, inplace=True),
+            nn.Dropout(p=dropout_prob)
+        )
 
     def forward(self, x):
         enc1 = self.enc1(x)
@@ -77,26 +25,47 @@ class UNet(nn.Module):
         enc3 = self.enc3(enc2)
         enc4 = self.enc4(enc3)
         bottleneck = self.bottleneck(enc4)
+        return bottleneck, enc1, enc2, enc3, enc4
+
+class YOLODecoder(nn.Module):
+    def __init__(self, num_classes):
+        super(YOLODecoder, self).__init__()
         
-        up4 = self.upconv4(bottleneck)
-        up4 = F.interpolate(up4, size=enc4.size()[2:], mode='bilinear', align_corners=True)
-        up4 = torch.cat([up4, enc4], dim=1)
-        up4 = self.conv_up4(up4)
+        self.upconv1 = self.upconv_block(512 + 256, 256)  # Skip connection with enc4
+        self.upconv2 = self.upconv_block(256 + 128, 128)  # Skip connection with enc3
+        self.upconv3 = self.upconv_block(128 + 64, 64)    # Skip connection with enc2
+        self.upconv4 = self.upconv_block(64 + 32, 32)     # Skip connection with enc1
+        
+        self.final_conv = nn.Conv2d(32, num_classes + 5, kernel_size=1)  # (x, y, w, h, conf) + class scores
 
-        up3 = self.upconv3(up4)
-        up3 = F.interpolate(up3, size=enc3.size()[2:], mode='bilinear', align_corners=True)
-        up3 = torch.cat([up3, enc3], dim=1)
-        up3 = self.conv_up3(up3)
+    def upconv_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.1, inplace=True)
+        )
 
-        up2 = self.upconv2(up3)
-        up2 = F.interpolate(up2, size=enc2.size()[2:], mode='bilinear', align_corners=True)
-        up2 = torch.cat([up2, enc2], dim=1)
-        up2 = self.conv_up2(up2)
+    def forward(self, x, enc1, enc2, enc3, enc4):
+        x = self.upconv1(torch.cat([x, enc4], dim=1))
+        x = self.upconv2(torch.cat([x, F.interpolate(enc3, size=x.size()[2:], mode='bilinear', align_corners=True)], dim=1))
+        x = self.upconv3(torch.cat([x, F.interpolate(enc2, size=x.size()[2:], mode='bilinear', align_corners=True)], dim=1))
+        x = self.upconv4(torch.cat([x, F.interpolate(enc1, size=x.size()[2:], mode='bilinear', align_corners=True)], dim=1))
+        x = self.final_conv(x)
 
-        up1 = self.upconv1(up2)
-        up1 = F.interpolate(up1, size=enc1.size()[2:], mode='bilinear', align_corners=True)
-        up1 = torch.cat([up1, enc1], dim=1)
-        up1 = self.conv_up1(up1)
+        # Apply YOLO-style activations
+        x[:, 0:2, :, :] = torch.sigmoid(x[:, 0:2, :, :])  # (x, y) -> range [0,1]
+        x[:, 2:4, :, :] = torch.exp(x[:, 2:4, :, :])      # (w, h) -> positive values
+        x[:, 4:, :, :] = torch.sigmoid(x[:, 4:, :, :])    # (conf + class scores) -> range [0,1]
 
-        output = self.output(up1)
-        return output
+        return x
+
+class UNetYOLO(nn.Module):
+    def __init__(self, in_channels, num_classes, dropout_prob=0.3):
+        super(UNetYOLO, self).__init__()
+        self.encoder = UNetEncoder(in_channels, dropout_prob)
+        self.decoder = YOLODecoder(num_classes)
+
+    def forward(self, x):
+        bottleneck, enc1, enc2, enc3, enc4 = self.encoder(x)
+        output = self.decoder(bottleneck, enc1, enc2, enc3, enc4)
+        return output  # YOLO-style detection output
